bb60d727f767a101abbbc64ff480f7a41184ea61
AbaMatta <matthewm@abadata.ca>
2025-11-26 22:16:30 -0700

    Sliders

--- src/cython/cychess.pyx
+++ src/cython/cychess.pyx
@@ -12,11 +12,11 @@
 from libc.stdint cimport uint64_t, uint8_t, uint16_t, int8_t
 
 # Piece constants (C-level)
-cdef uint8_t PIECE_NONE = 0
-cdef uint8_t PIECE_WP = 1, PIECE_WN = 2, PIECE_WB = 3, PIECE_WR = 4
-cdef uint8_t PIECE_WQ = 5, PIECE_WK = 6
-cdef uint8_t PIECE_BP = 7, PIECE_BN = 8, PIECE_BB = 9, PIECE_BR = 10
-cdef uint8_t PIECE_BQ = 11, PIECE_BK = 12
+cdef public uint8_t PIECE_NONE = 0
+cdef public uint8_t PIECE_WP = 1, PIECE_WN = 2, PIECE_WB = 3, PIECE_WR = 4
+cdef public uint8_t PIECE_WQ = 5, PIECE_WK = 6
+cdef public uint8_t PIECE_BP = 7, PIECE_BN = 8, PIECE_BB = 9, PIECE_BR = 10
+cdef public uint8_t PIECE_BQ = 11, PIECE_BK = 12
 
 # Castling
 cdef uint8_t CASTLE_WK = 1, CASTLE_WQ = 2, CASTLE_BK = 4, CASTLE_BQ = 8
@@ -39,9 +39,21 @@ PAWN_CAP_EAST[:] = [9, -7]
 cdef int KING_DELTAS[8]
 KING_DELTAS[:] = [-9, -8, -7, -1, 1, 7, 8, 9]
 
+# Slider directions
+cdef int ROOK_DIRECTIONS[4]
+ROOK_DIRECTIONS[:] = [8, -8, 1, -1]  # N S E W
+
+cdef int BISHOP_DIRECTIONS[4]
+BISHOP_DIRECTIONS[:] = [9, 7, -7, -9]  # NE NW SW SE
+
+cdef int QUEEN_DIRECTIONS[8]
+QUEEN_DIRECTIONS[:] = [8, -8, 1, -1, 9, 7, -7, -9]  # Rook + Bishop
+
 # Free inline functions (pure C, GIL-free)
 cdef inline uint64_t sq_to_bit(int sq) nogil:
     return 1ULL << sq
+
+cpdef uint64_t py_sq_to_bit(int sq): return sq_to_bit(sq)
 
 cdef struct Move:
     uint8_t fr_sq   # from square (0-63)
@@ -58,9 +70,9 @@ cdef class Board:
     cdef int move_count
 
     def __cinit__(self):
-        self.clear()
+        self._clear()
 
-    cdef void clear(self) nogil:
+    cdef void _clear(self) nogil:
         cdef int i
         for i in range(12):
             self.pieces[i] = 0ULL
@@ -70,6 +82,9 @@ cdef class Board:
         self.ep_square = -1
         self.halfmove = 0
         self.fullmove = 1
+
+    cpdef void clear(self):
+        self._clear()
 
     cdef void _update_occupancy(self) nogil:
         cdef uint64_t white = 0ULL
@@ -228,6 +243,138 @@ cdef class Board:
                             self.move_count += 1
         return self.move_count
 
+    cdef int _generate_rook_moves(self, uint64_t rooks_bb, uint64_t own_occ, uint64_t opp_occ) nogil:
+        cdef uint64_t blockers = own_occ | opp_occ
+        cdef int sq, dir_idx, target, dir_delta
+        cdef uint64_t target_bb
+        cdef int dfile, drank
+        cdef bint vertical, horizontal
+
+        for sq in range(64):
+            if (sq_to_bit(sq) & rooks_bb) == 0:
+                continue
+            for dir_idx in range(4):
+                if self.move_count >= 256:
+                    return self.move_count
+                dir_delta = ROOK_DIRECTIONS[dir_idx]
+                target = sq + dir_delta
+                while 0 <= target < 64:
+                    dfile = (target & 7) - (sq & 7)
+                    drank = (target // 8) - (sq // 8)
+                    vertical = (dir_delta == 8 or dir_delta == -8)
+                    horizontal = (dir_delta == 1 or dir_delta == -1)
+                    if vertical:
+                        if (target & 7) != (sq & 7):
+                            break
+                    elif horizontal:
+                        if (target // 8) != (sq // 8):
+                            break
+                    else:
+                        if abs(dfile) != abs(drank):
+                            break  # Won't trigger for rook dirs
+
+                    target_bb = sq_to_bit(target)
+                    if (target_bb & blockers) != 0:
+                        if (target_bb & opp_occ) != 0:
+                            self.moves[self.move_count].fr_sq = <uint8_t>sq
+                            self.moves[self.move_count].to_sq = <uint8_t>target
+                            self.move_count += 1
+                        break
+                    else:
+                        self.moves[self.move_count].fr_sq = <uint8_t>sq
+                        self.moves[self.move_count].to_sq = <uint8_t>target
+                        self.move_count += 1
+                    target += dir_delta
+        return self.move_count
+
+    cdef int _generate_bishop_moves(self, uint64_t bishops_bb, uint64_t own_occ, uint64_t opp_occ) nogil:
+        cdef uint64_t blockers = own_occ | opp_occ
+        cdef int sq, dir_idx, target, dir_delta
+        cdef uint64_t target_bb
+        cdef int dfile, drank
+        cdef bint vertical, horizontal
+
+        for sq in range(64):
+            if (sq_to_bit(sq) & bishops_bb) == 0:
+                continue
+            for dir_idx in range(4):
+                if self.move_count >= 256:
+                    return self.move_count
+                dir_delta = BISHOP_DIRECTIONS[dir_idx]
+                target = sq + dir_delta
+                while 0 <= target < 64:
+                    dfile = (target & 7) - (sq & 7)
+                    drank = (target // 8) - (sq // 8)
+                    vertical = (dir_delta == 8 or dir_delta == -8)
+                    horizontal = (dir_delta == 1 or dir_delta == -1)
+                    if vertical:
+                        if (target & 7) != (sq & 7):
+                            break
+                    elif horizontal:
+                        if (target // 8) != (sq // 8):
+                            break
+                    else:
+                        if abs(dfile) != abs(drank):
+                            break
+
+                    target_bb = sq_to_bit(target)
+                    if (target_bb & blockers) != 0:
+                        if (target_bb & opp_occ) != 0:
+                            self.moves[self.move_count].fr_sq = <uint8_t>sq
+                            self.moves[self.move_count].to_sq = <uint8_t>target
+                            self.move_count += 1
+                        break
+                    else:
+                        self.moves[self.move_count].fr_sq = <uint8_t>sq
+                        self.moves[self.move_count].to_sq = <uint8_t>target
+                        self.move_count += 1
+                    target += dir_delta
+        return self.move_count
+
+    cdef int _generate_queen_moves(self, uint64_t queens_bb, uint64_t own_occ, uint64_t opp_occ) nogil:
+        cdef uint64_t blockers = own_occ | opp_occ
+        cdef int sq, dir_idx, target, dir_delta
+        cdef uint64_t target_bb
+        cdef int dfile, drank
+        cdef bint vertical, horizontal
+
+        for sq in range(64):
+            if (sq_to_bit(sq) & queens_bb) == 0:
+                continue
+            for dir_idx in range(8):
+                if self.move_count >= 256:
+                    return self.move_count
+                dir_delta = QUEEN_DIRECTIONS[dir_idx]
+                target = sq + dir_delta
+                while 0 <= target < 64:
+                    dfile = (target & 7) - (sq & 7)
+                    drank = (target // 8) - (sq // 8)
+                    vertical = (dir_delta == 8 or dir_delta == -8)
+                    horizontal = (dir_delta == 1 or dir_delta == -1)
+                    if vertical:
+                        if (target & 7) != (sq & 7):
+                            break
+                    elif horizontal:
+                        if (target // 8) != (sq // 8):
+                            break
+                    else:
+                        if abs(dfile) != abs(drank):
+                            break
+
+                    target_bb = sq_to_bit(target)
+                    if (target_bb & blockers) != 0:
+                        if (target_bb & opp_occ) != 0:
+                            self.moves[self.move_count].fr_sq = <uint8_t>sq
+                            self.moves[self.move_count].to_sq = <uint8_t>target
+                            self.move_count += 1
+                        break
+                    else:
+                        self.moves[self.move_count].fr_sq = <uint8_t>sq
+                        self.moves[self.move_count].to_sq = <uint8_t>target
+                        self.move_count += 1
+                    target += dir_delta
+        return self.move_count
+
     cdef int generate_pseudo_legal_moves(self) nogil:
         self.move_count = 0
         cdef bint is_white = self.white_to_move
@@ -246,6 +393,18 @@ cdef class Board:
         # Kings
         cdef uint64_t kings_bb = self.pieces[PIECE_WK - 1 if is_white else PIECE_BK - 1]
         self._generate_king_moves(kings_bb, own_occ, opp_occ)
+
+        # Rooks
+        cdef uint64_t rooks_bb = self.pieces[PIECE_WR - 1 if is_white else PIECE_BR - 1]
+        self._generate_rook_moves(rooks_bb, own_occ, opp_occ)
+
+        # Bishops
+        cdef uint64_t bishops_bb = self.pieces[PIECE_WB - 1 if is_white else PIECE_BB - 1]
+        self._generate_bishop_moves(bishops_bb, own_occ, opp_occ)
+
+        # Queens
+        cdef uint64_t queens_bb = self.pieces[PIECE_WQ - 1 if is_white else PIECE_BQ - 1]
+        self._generate_queen_moves(queens_bb, own_occ, opp_occ)
 
         # TODO: sliders (rooks/bishops/queens), castling, EP, promotions
         return self.move_count
--- src/scripts/testCychess.py
+++ src/scripts/testCychess.py
@@ -1,7 +1,11 @@
-from cychess import Board  # adjust import
+from cychess import Board, py_sq_to_bit as sq_to_bit  # adjust import
 board = Board()
 board.set_start_position()
 moves = board.get_moves_list()
 print(f"Move count: {len(moves)}")  # Expect 20
 print("Sample pawn moves:", [m for m in moves[:4] if m[0] // 8 == 1])  # e2-e3/e4 etc.
 # Knights: e.g., (1, 16), (1, 18), (6, 17), (6, 21)
+
+board.clear()
+board.pieces[4] = sq_to_bit(0)  # a1 rook (need sq_to_bit Python wrapper?)
+print(len(board.get_moves_list()))
